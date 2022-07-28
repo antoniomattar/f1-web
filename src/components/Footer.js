3940af0b10a813abba6994009d7398012cb202eb
Antonio Mattar <antoniomattar123@gmail.com>
2022-07-28 13:16:44 +0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 
     return (
-        <span style={{ display:'flex', justifyContent:'center',fontFamily: 'monospace', textAlign: 'center'}}>
+        <span style={{ display:'flex', justifyContent:'center',fontFamily: 'monospace', textAlign: 'center', margin:"2"}}>
         Made with&nbsp;<strong style={{color: 'red'}}>&hearts;</strong>&nbsp;by&nbsp;<strong><a style={{color:'red'}}href="https://www.instagram.com/antonio_mattar/"> <u>@antonio_mattar</u></a></strong>
         </span>
           
