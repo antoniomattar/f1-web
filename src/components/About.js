1d9b17ea438a3b6d190aa72067a895eb6b81ee42
Antonio Mattar <antoniomattar123@gmail.com>
2022-07-28 12:53:31 +0300

    ....

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,12 @@ const About = () => {
   return (
     <React.Fragment>
     
-    <Text fontSize='20px' color='black' margin="10">
-      Hello World, 
+    <Text fontSize='20px' color='black' margin="50">
+      Hello World,
+      <br/>
       I am just a young developper with big dreams who loves watching sports.
       Tech Lover + Sport Lover = ? Why not a sports streaming site!
+      <br/>
       I will be trying to cover the maximum of games with the least of ads. Although ads are disgusting but at a certain point I will really need them to pursue paying the hosting costs.
       Thank you!
     </Text>
