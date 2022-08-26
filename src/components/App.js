437fab88cb025df9b48112af89ef02d8d90a7a8f
antoniomattar <antoniomattar123@icloud.com>
2022-08-26 15:15:21 +0300

    fixed f1 eng

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ function App() {
       <Menu/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<F1English name='English' link= 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'/>}/>
+          <Route exact path="/" element={<F1English name='English' link= 'https://f5b2ccf6d2fa.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.438551474672.channel.1PFmwnAW7tfl.m3u8'/>}/>
           <Route exact path="/arabic" element={<F1Arabic name='Arabic' link= 'https://www.youtube.com/watch?v=9jo51nJrO0k'/>}/>
           <Route exact path="/basketball" element={<Basket name='Basket' link= 'https://cllive.itworkscdn.net/lbcdramalive/token=nva=1661441291~dirs=1~hash=09cdb8f1f14cb460cc1d0/drama/lb2_360p_chunks.m3u8'/>}/>
           <Route exact path="/about" element={<About/>}/>
