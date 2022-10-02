51252122be39f966d5a44953ac9cced673ea43c1
antoniomattar <antoniomattar123@icloud.com>
2022-10-02 16:44:38 +0200

    AAA

--- src/components/App.js
+++ src/components/App.js
@@ -20,8 +20,8 @@ function App() {
       <Menu/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<F1English name='English' link= 'https://b8c302723ffc.us-east-1.playback.live-video.net/api/video/v1/us-east-1.403265903508.channel.CKYZrZo2EWNc.m3u8'/>}/>
-          <Route exact path="/arabic" element={<F1Arabic name='Arabic' link= 'https://b8c302723ffc.us-east-1.playback.live-video.net/api/video/v1/us-east-1.403265903508.channel.CKYZrZo2EWNc.m3u8'/>}/>
+          <Route exact path="/" element={<F1English name='English' link= 'https://258beb306854.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.403265903508.channel.k4eGDri26pOY.m3u8'/>}/>
+          <Route exact path="/arabic" element={<F1Arabic name='Arabic' link= 'https://258beb306854.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.403265903508.channel.k4eGDri26pOY.m3u8'/>}/>
           <Route exact path="/basketball" element={<Basket name='Basket' link= 'https://cllive.itworkscdn.net/lbcdramalive/token=nva=1661785795~dirs=1~hash=01d143e9f08fff5fdc7cc/drama/lb2_360p_chunks.m3u8'/>}/>
           <Route exact path="/about" element={<About/>}/>
         </Routes>
