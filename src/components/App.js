1eab1a1e151e06be56dc8699d57a6d31f1d93342
antoniomattar <antoniomattar123@icloud.com>
2022-10-22 20:52:59 +0200

    AAA

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ function App() {
       <Menu/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<F1English name='English' link= 'http://himes.wesah.xyz/scripts/NTY5MA==/1080p.m3u8'/>}/>
+          <Route exact path="/" element={<F1English name='English' link= 'https://webudi.openhd.lol/ddy5/premium60/tracks-v1a1/mono.m3u8'/>}/>
           <Route exact path="/arabic" element={<F1Arabic name='Arabic' link= 'https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master_1660.m3u8'/>}/>
           <Route exact path="/basketball" element={<Basket name='Basket' link= 'https://cllive.itworkscdn.net/lbcdramalive/token=nva=1661785795~dirs=1~hash=01d143e9f08fff5fdc7cc/drama/lb2_360p_chunks.m3u8'/>}/>
           <Route exact path="/about" element={<About/>}/>
