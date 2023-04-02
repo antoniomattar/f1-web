b683f177e5162dc9c3e0dcb7d74ea8eb1554ddac
antoniomattar <antoniomattar123@icloud.com>
2023-04-02 06:56:58 +0200

    go

--- src/components/F1 English.js
+++ src/components/F1 English.js
@@ -9,6 +9,8 @@ import { Button, Center, ChakraProvider,Divider} from "@chakra-ui/react";
 import M3U8Player from "./M3U8Player";
 
 
+// 5759078047346
+// 5759078440562
 const F1English = (props) => {
 
   return (
@@ -20,13 +22,13 @@ const F1English = (props) => {
             <Title name="STREAM 1"/>
 
             <Center>
-            <iframe width="640" height="360" src="//ok.ru/videoembed/5759068085874?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+            <M3U8Player link="https://ed-t1.edghst.me/plyvivo/wexuvug0p040gu20veka/chunklist.m3u8"/>
             </Center>
 
             <Title name="STREAM 2"/>
 
             <Center>
-            <M3U8Player link="https://ds1.sportea.online/stream/ch10/index.fmp4.m3u8"/>
+            <iframe width="640" height="360" src="//ok.ru/videoembed/5759068085874?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
             </Center>
 
 
