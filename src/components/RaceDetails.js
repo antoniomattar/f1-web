c9a5eebd9473a26659d15ab5722deaebaf488379
antoniomattar <antoniomattar123@icloud.com>
2023-04-29 15:32:19 +0200

    fixed smthg

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -35,13 +35,12 @@ class RaceDetails extends Component {
       <div className="grid justify-center items-center align-middle">
         <h1 className="text-3xl font-bold text-center">🏎️ RACE DETAILS 🏎️</h1>
         <div className=" bg-green-700 text-white rounded-2xl p-5">
-          <h2 className=" text-xl font-semibold">NAME: {this.state.closestRace ? this.state.closestRace["raceName"] : "Loading..."} </h2>
+          <h2 className=" text-xl font-semibold">NAME: {this.state.closestRace ? this.state.closestRace["raceName"] +" "+ this.state.closestRace["season"] : "Loading..."} </h2>
           <h3 className=" text-sm font-semibold">CIRCUIT NAME: {this.state.closestRace ? this.state.closestRace["Circuit"]["circuitName"] : "Loading..."} </h3>
           <h3 className=" text-sm font-semibold">LOCATION: {this.state.closestRace ? this.state.closestRace["Circuit"]["Location"]["locality"] : "Loading..."} </h3>
           <h3 className=" text-sm font-semibold">COUNTRY: {this.state.closestRace ? this.state.closestRace["Circuit"]["Location"]["country"] : "Loading..."} </h3>
           <h3 className=" text-sm font-semibold">DATE: {this.state.closestRace ? this.state.closestRace["date"] : "Loading..."} </h3>
-          <h3 className=" text-sm font-semibold">TIME: {this.state.closestRace ? this.state.closestRace["time"] : "Loading..."} </h3>
-          <h3 className=" text-sm font-semibold">SEASON {this.state.closestRace ? this.state.closestRace["season"] : "Loading..."} </h3>
+          <h3 className=" text-sm font-semibold">LOCAL TIME: {this.state.closestRace ? this.state.closestRace["time"].substring(0,this.state.closestRace["time"].length -1) : "Loading..."} </h3>
         </div>
       </div>
       
