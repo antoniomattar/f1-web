ba2f612baed0385bd4f456a2c5359a7e5aadc77f
antoniomattar <antoniomattar123@icloud.com>
2023-04-29 10:38:55 +0200

    update

--- src/components/F1 Arabic.js
+++ src/components/F1 Arabic.js
@@ -11,7 +11,7 @@ const F1Arabic = (props) => {
     <div>
             <h1 className=" mx-auto">🏎️  Arabic Stream  🏎️</h1>
             <WhatsAppGrpBtn/>
-            <iframe className='border-solid border-8 border-red-600 rounded-xl mx-auto m-6' width="640" height="360" src="//ok.ru/videoembed/5529139748466?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>
+            <iframe className='border-solid border-8 border-red-600 rounded-xl mx-auto m-6' width="640" height="360" src="//ok.ru/videoembed/4855918829257?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>
             <RefreshBtn/>
             <StreamChangeButton language='Arabic' newlang='English'/>
             <WhatsAppChatBtn/>
