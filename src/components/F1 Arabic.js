a324eb4bdd1c121600501c31268971099e082d28
antoniomattar <antoniomattar123@icloud.com>
2023-04-29 10:41:57 +0200

    fixed

--- src/components/F1 Arabic.js
+++ src/components/F1 Arabic.js
@@ -8,10 +8,10 @@ import WhatsAppChatBtn from "./WhatsAppChatBtn";
 const F1Arabic = (props) => {
 
   return (
-    <div>
+    <div className="grid justify-center">
             <h1 className=" mx-auto">🏎️  Arabic Stream  🏎️</h1>
             <WhatsAppGrpBtn/>
-            <iframe className='border-solid border-8 border-red-600 rounded-xl mx-auto m-6' width="640" height="360" src="//ok.ru/videoembed/4855918829257?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>
+            <iframe  className=" w-fit border-solid border-8 border-red-600 rounded-xl m-6" src="//ok.ru/videoembed/4855918829257?nochat=1&autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>
             <RefreshBtn/>
             <StreamChangeButton language='Arabic' newlang='English'/>
             <WhatsAppChatBtn/>
