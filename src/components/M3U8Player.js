d61895ce010ce77ba5444a4f9c80ec4e955c9f2e
antoniomattar <antoniomattar123@icloud.com>
2023-04-29 11:10:09 +0200

    gogo fixed chatango

--- src/components/M3U8Player.js
+++ src/components/M3U8Player.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
-import { Center } from '@chakra-ui/react';
 
 const M3U8Player = (props) => {
   
   return (
     <>
-    <Center>
       <div className='mx-auto w-fit border-solid border-8 border-red-600 rounded-xl m-6'>
         <ReactPlayer url={props.link} playing={true} controls={true} width={200} height={200} />
       </div>
-    </Center>
     </>
   )
 }
