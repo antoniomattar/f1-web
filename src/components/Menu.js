428e2c756182b6682daeb0761b244cf091150e17
antoniomattar <antoniomattar123@icloud.com>
2023-04-30 12:16:50 +0200

    made the menu full responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,36 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
 import f1logo from '../assets/logo.jpeg';
+import React from 'react';
+import Divider from './Divider';
+
 
 const Menu = () => {
+  function menuToggle() {
+    const menu = document.querySelector("#menu-toggle");
+    menu.classList.toggle("hidden");
+    }
+
   return (
     <>
-      {/* <nav class=" bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 mb-10 px-2">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-  <a href="./" class="flex items-center">
-      <img src={f1logo} class=" h-11  mr-3" alt="Flowbite Logo"/>
-  </a>
-  <div class="flex md:order-2">
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      </button>
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <a href="./" class="block py-2 pl-3 pr-4 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 md:dark:text-red-500" aria-current="page">Home 🏎️</a>
-      </li>
-      <li>
-        <a href="./archive" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Archives</a>
-      </li>
-      <li>
-        <a href="./about" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav> */}
       <nav class="border-b border-gray-200 px-2">
         <div class="mx-auto flex max-w-screen-xl flex-wrap justify-center p-4">
           <a href="./" class="flex items-center">
@@ -38,6 +19,8 @@ const Menu = () => {
           </a>
           <div class="flex md:order-2">
             <button
+              onClick={menuToggle}
+              id='menu-button'
               data-collapse-toggle="navbar-sticky"
               type="button"
               class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
@@ -85,7 +68,7 @@ const Menu = () => {
               <li>
                 <a
                   href="./about"
-                  class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-red-700 md:dark:hover:bg-transparent md:dark:hover:text-red-500"
+                  class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-red-700 md:dark:hover:bg-transparent md:dark:hover:text-red-700"
                 >
                   About
                 </a>
@@ -93,6 +76,34 @@ const Menu = () => {
             </ul>
           </div>
         </div>
+        <div class="hidden w-full md:items-center md:w-auto text-center" id="menu-toggle">
+          <Divider/>
+          <ul
+            class="
+              pt-2
+              text-base text-gray-700
+              md:flex
+              md:justify-between 
+              md:pt-0"
+          >
+            <li>
+              <a class="md:p-4 py-2 block hover:text-red-700" href="./"
+                >Home 🏎️</a>
+              
+            </li>
+            <li>
+              <a class="md:p-4 py-2 block hover:text-red-700" href="./archive"
+                >Archives</a>
+
+            </li>
+            <li>
+              <a class="md:p-4 py-2 block hover:text-red-700" href="./about"
+                >About</a>
+              
+            </li>
+          </ul>
+        </div>
+        
       </nav>
     </>
   );
