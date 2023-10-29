9f097937e2adeca24a5ce40e74816f61c2ae2836
antoniomattar <antoniomattar123@icloud.com>
2023-10-29 21:22:13 +0100

    gogo

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ export default function App() {
               <F1English
                 name="English"
                 link2="https://a36.azplay.me/hls/streama216039/index.m3u8?cst=6c7526271d149ee774a7bef36638745d"
-                link1="https://sportsleading.online/live/stream_f1.m3u8"
+                link1="https://a36.azplay.me/hls/streama216206/index.m3u8?cst=137ad2b6e0cc18908de55820a44e7677"
               />
             }
           />
