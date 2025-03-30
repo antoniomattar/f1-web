980626631699a5452d576361fea46e9ef5bf9c87
Antonio MATTAR <antonio.mattar@grenoble-inp.org>
2025-03-30 19:54:28 +0200

    Fixed the link

--- src/components/F1 English.js
+++ src/components/F1 English.js
@@ -6,7 +6,7 @@ export default function F1English() {
     <div className="bg-gray-100 text-gray-800 py-10 px-6">
       <h1 className="text-2xl font-bold mb-4 text-center">F1 English Stream</h1>
       <IframePlayer
-        src="https://stream-fastly.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/index.fmp4.m3u8"
+        src="https://wikihznew.koskoros.ru/wikihz/wiki30/mono.m3u8"
         title="F1 English Stream"
       />
     </div>
