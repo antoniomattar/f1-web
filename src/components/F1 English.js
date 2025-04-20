2a0d6355f6071aa0f3050d3e99d680b6e8a4c60e
Antonio MATTAR <antonio.mattar@grenoble-inp.org>
2025-04-20 19:33:00 +0200

    update

--- src/components/F1 English.js
+++ src/components/F1 English.js
@@ -6,7 +6,7 @@ export default function F1English() {
     <div className="bg-gray-100 text-gray-800 py-10 px-6">
       <h1 className="text-2xl font-bold mb-4 text-center">F1 English Stream</h1>
       <IframePlayer
-        src="https://wikihznew.koskoros.ru/wikihz/wiki30/mono.m3u8"
+        src="https://c58bx6vat7sxiexj.freshsoup.shop/v4/variant/VE1gTdz0mLzRnLv52bt9SMhFjdtM3ajFmc09SZlFjZlFGN1gjM0MWL5I2M50iNhdDNtAzYyUTLmlTZ5ITZjN2L.m3u8?md5=kv1Ew8ekZggT4P-WuEpTjA&expires=1745213437"
         title="F1 English Stream"
       />
     </div>
